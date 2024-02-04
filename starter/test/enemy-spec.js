eb7fdaaf2cb933ab42a3bb6ab5ba2795a88d9765
Job Jean-Pierre Joseph <jobjpjoseph@gmail.com>
2024-02-04 05:14:16 -0500

    We got rid of a set of tests be it did not follow the DRY

--- starter/test/enemy-spec.js
+++ starter/test/enemy-spec.js
@@ -45,16 +45,6 @@ describe ('Character', function () {
   });
 
   it('should drop all held items and have currentRoom set to null when dead', function () {
-    // console.log(room)
-    // console.log(character.currentRoom);
-    // console.log(room === character.currentRoom);
-
-    // character.die();
-
-    // console.log(room)
-    // console.log(character.currentRoom);
-    // console.log(room === character.currentRoom);
-
     expect(character.currentRoom).to.equal(room);
     expect(room.items.length).to.equal(0);
     character.die();
@@ -77,67 +67,6 @@ describe ('Character', function () {
   });
 
 });
-
-describe ('Enemy', function () {
-
-  let player;
-  let enemy;
-  let room;
-  let item;
-
-  beforeEach(function() {
-    room =  new Room("Test Room", "A test room");
-    item = new Item("rock", "just a simple rock");
-    character = new Character('Character', 'an ordinary character', room);
-    character.items.push(item);
-  });
-
-  it('should have name and description attributes', function () {
-    expect(character.name).to.equal("Character");
-    expect(character.description).to.equal('an ordinary character');
-  });
-
-  it('should not be an instance of Enemy or Player', function () {
-    expect(character instanceof Player).to.be.false;
-    expect(character instanceof Enemy).to.be.false;
-  });
-
-  it('should have strength and health attributes', function () {
-    expect(character.health).to.equal(100);
-    expect(character.strength).to.equal(10);
-  });
-
-  it('should lose health when damage is applied', function () {
-    expect(character.health).to.equal(100);
-    character.applyDamage(10);
-    expect(character.health).to.equal(90);
-  });
-
-  it('should drop all held items and have currentRoom set to null when dead', function () {
-    expect(character.currentRoom).to.equal(room);
-    expect(room.items.length).to.equal(0);
-    character.die();
-    expect(character.currentRoom).to.equal(null);
-    expect(room.items.length).to.equal(1);
-    expect(room.items[0]).to.equal(item);
-  });
-
-  it('should die when damage brings health to 0 or less', function () {
-    expect(character.currentRoom).to.equal(room);
-    expect(room.items.length).to.equal(0);
-
-    expect(character.health).to.equal(100);
-    character.applyDamage(100);
-    expect(character.health).to.equal(0);
-
-    expect(character.currentRoom).to.equal(null);
-    expect(room.items.length).to.equal(1);
-    expect(room.items[0]).to.equal(item);
-  });
-
-});
-
-
 
 describe ('Enemy', function () {
 
