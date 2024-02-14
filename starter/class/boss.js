aa7d53ea180194b2979fecedd4a97056d6233abd
Job Jean-Pierre Joseph <jobjpjoseph@gmail.com>
2024-02-14 00:25:40 -0500

    We finished coding the constructor

--- starter/class/boss.js
+++ starter/class/boss.js
@@ -1,7 +1,13 @@
 const { Enemy } = require('./enemy');
 
-class Boss {
+class Boss extends Enemy {
 
+    constructor(name, description, goblin) {
+        super(name, description);
+        this.strength = ((goblin.strength * 1.30) + goblin.strength);
+        this.health = ((goblin.health * 1.15) + goblin.health);
+        this.isBoss = true;
+    }
 }
 
 module.exports = {
